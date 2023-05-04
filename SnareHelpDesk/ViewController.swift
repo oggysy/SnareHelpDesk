7ca8923af2b16650e5849ecc5799ee9cb0b2b191
oggysy <ogisoys@gmail.com>
2023-05-04 15:20:30 +0900

    Initial Commit

---
+++ SnareHelpDesk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnareHelpDesk
+//
+//  Created by 小木曽佑介 on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
